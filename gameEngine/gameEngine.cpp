051b0ec004cb3849a8354821ae58154d52cd966b
Canadastone <157530446+Canadastone@users.noreply.github.com>
2025-09-25 11:58:54 -0400

    Base Template
    Just the base skeleton for how the project will be structured

---
+++ gameEngine/gameEngine.cpp
@@ -0,0 +1,13 @@
+/*
+* Assignment 1 - COMP 345
+* Authors: Ariberto Bello Veras, Joshua Bitton, Liam Daigle, 
+* Ash Stone, Cyrus Stonebanks, Kirill Vishnyakov
+* Date: 2025-10-07
+* Description: This file contains the implementation of the Game's Engine
+*/
+
+#include "gameEngine.h"
+#include <iostream>
+using namespace std;
+
+// TODO: Create a group of classes to represent the different phases of the game
---
+++ main/mainDriver.cpp
@@ -0,0 +1,16 @@
+/*
+* Assignment 1 - COMP 345
+* Authors: Ariberto Bello Veras, Joshua Bitton, Liam Daigle, 
+* Ash Stone, Cyrus Stonebanks, Kirill Vishnyakov
+* Date: 2025-10-07
+* Description: This is the main driver file for the Warzone game.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    return 0;
+
+}
