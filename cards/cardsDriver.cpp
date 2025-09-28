c14a3c9be319b8be6a7801d68cc9800217431b19
Joshua Bitton <joshtosh13@hotmail.com>
2025-09-28 15:46:51 -0400

    Implemented cards feature
    Implemented the Deck class, hand class and card class with necessary methods.
    For the Deck, a deck configuration was created so you can easily modify the number of cards in the deck.
    Deck:: Implemented the draw, and return card to deck methods
    Hand:: Implemented the add card, and use card in hand methods
    Card:: Implemented the play card method

--- cards/cardsDriver.cpp
+++ cards/cardsDriver.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 void testCards() {
 
-    cout << "Testing Cards" << endl;
-
+    cout << "Drawing from deck: \n" << endl;
+    Deck deck;
+    Hand hand;
+    while ( deck.size() != 0 ) {
+        deck.draw(hand);
+    }
+    cout << "Deck size after drawing is: " << deck.size() << endl;
+    cout << "Playing cards in hand: \n" << endl;
+    while ( hand.size() != 0 ) {
+        shared_ptr<Card> card = *(hand.getHand().begin());
+        hand.useCard(card, deck);
+    }
+    cout << "Deck size after playing is: " << deck.size() << endl;
 };
