83c87d3f0589560488c7c00b9b3859ef7b44d5c2
Canadastone <157530446+Canadastone@users.noreply.github.com>
2025-10-04 17:59:59 -0400

    Player update
    Most recent patch for player implementation

--- player/player.cpp
+++ player/player.cpp
@@ -8,25 +8,60 @@
 
 #include "player.h"
 #include <iostream>
-#include "orders/orders.h"
-#include "map/map.h"
-#include "cards/cards.h"
+#include <list>
 
 using namespace std;
 
 // TODO: Implement the player logic here
 
 
-class player{
-
-    private:
-        
-
-    public:
-        void toDefend();
-        void toAttack();
-        void issueOrder();
-
+Player::Player() {
     
+    cout << "Player created" << endl;  
 
-};
+}
+Player::~Player() {
+    // Destructor logic if needed
+}
+void Player::toDefend() {
+    cout << "Player toDefend method called" << endl;
+    // Placeholder implementation
+}
+void Player::toAttack() {
+    cout << "Player toAttack method called" << endl;
+    // Placeholder implementation
+}
+void Player::issueOrder() {
+    cout << "Player issueOrder method called" << endl;
+    // Placeholder implementation
+}
+
+void Player::addTerritory(Territory t) {
+    cout << "Adding territory to player" << endl;
+    playerTerritories->push_back(t);
+}
+
+void Player::removeTerritory(Territory t) {
+    cout << "Removing territory from player" << endl;
+    playerTerritories->remove(t);
+}
+
+void Player::addCard(Card c) {
+    cout << "Adding card to player" << endl;
+    //TODO: add card to player's hand here
+}
+
+void Player::removeCard(Card c) {
+    cout << "Removing card from player" << endl;
+    //TODO: remove card from player's hand here
+}
+
+void Player::addOrder(Order o) {
+    cout << "Adding order to player" << endl;
+    playerOrders->push_back(o);
+}
+
+void Player::removeOrder(Order o) {
+    cout << "Removing order from player" << endl;
+    playerOrders->remove(o);
+}
