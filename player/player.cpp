5f14e4395959138c54cc8d91c33e52e1daed6624
Canadastone <157530446+Canadastone@users.noreply.github.com>
2025-10-07 17:39:54 -0400

    Add files via upload

--- player/player.cpp
+++ player/player.cpp
@@ -7,6 +7,7 @@
 */
 
 #include "player.h"
+#include "../orders/orders.h"
 #include <iostream>
 #include <list>
 
@@ -18,128 +19,138 @@ using namespace std;
 Player::Player() {
     
     cout << "Player created" << endl;
-    playerHand = new Hand();
-    playerTerritories = new list<Map::Territory>();
-    playerOrders = new list<Order>();
+	playerHand = make_shared<Hand>();
 }
 
 Player::Player(const Player& other) {
     cout << "Player copy constructor called" << endl;
-    playerHand = new Hand(*other.playerHand);
-    playerTerritories = new list<Map::Territory>(*other.playerTerritories);
-    playerOrders = new list<Order>(*other.playerOrders);
+    playerHand = other.playerHand;
+    playerTerritories = other.playerTerritories;
+    playerOrders = other.playerOrders;
 }
 
-Player::~Player() {
-    delete playerHand;
-    delete playerTerritories;
-    delete playerOrders;
-}
-list<Map::Territory> Player::toDefend() {
+list<std::shared_ptr<Map::Territory>> Player::toDefend() {
 
     cout << "Player toDefend method called" << endl;
 
-    list<Map::Territory> defendList;
+    list<std::shared_ptr<Map::Territory>> defendList;
 
-    Map::Territory *t1 = new Map::Territory("Test Territory One", "Test Continent One");
-    Map::Territory *t2 = new Map::Territory("Test Territory Two", "Test Continent One");
+    std::shared_ptr<Map::Territory> t1 = make_shared<Map::Territory>("Test Territory One", "Test Continent One");
+    std::shared_ptr<Map::Territory> t2 = make_shared<Map::Territory>("Test Territory Two", "Test Continent One");
 
-    defendList.push_back(*t1);
-    defendList.push_back(*t2);
+    defendList.push_back(t1);
+    defendList.push_back(t2);
 
     return defendList;
     
 
 }
-list<Map::Territory> Player::toAttack() {
+list<std::shared_ptr<Map::Territory>> Player::toAttack() {
 
     cout << "Player toAttack method called" << endl;
     
-    list<Map::Territory> attackList;
+    list<std::shared_ptr<Map::Territory>> attackList;
 
-    Map::Territory *t1 = new Map::Territory("Test Territory One", "Test Continent One");
-    Map::Territory *t2 = new Map::Territory("Test Territory Two", "Test Continent One");
+    std::shared_ptr<Map::Territory> t1 = make_shared<Map::Territory>("Test Territory One", "Test Continent One");
+    std::shared_ptr<Map::Territory> t2 = make_shared<Map::Territory>("Test Territory Two", "Test Continent One");
 
-    attackList.push_back(*t1);
-    attackList.push_back(*t2);
+    attackList.push_back(t1);
+    attackList.push_back(t2);
 
     return attackList;
 
 }
-void Player::issueOrder() {
+void Player::issueOrder(shared_ptr<Order> o) {
 
     cout << "Player issueOrder method called" << endl;
-    
-    Order *o = new Order();
 
-    playerOrders->push_back(*o);
+    playerOrders.push_back(o);
 
     cout << "Order issued" << endl;
 
 }
 
-void Player::addTerritory(Map::Territory t) {
+void Player::addTerritory(shared_ptr<Map::Territory> t) {
     cout << "Adding territory to player" << endl;
-    playerTerritories->push_back(t);
+    playerTerritories.push_back(t);
 }
 
-void Player::removeTerritory(Map::Territory t) {
+void Player::removeTerritory(shared_ptr<Map::Territory> t) {
     cout << "Removing territory from player" << endl;
-    playerTerritories->remove(t);
+    playerTerritories.remove(t);
 }
 
-void Player::addCard(Card c) {
+void Player::addCard(shared_ptr<Card> c) {
     cout << "Adding card to player" << endl;
-    playerHand->addCard(std::make_shared<Card>(c));
-    //TODO: add card to player's hand here
+    playerHand->addCard(c);
+    
 }
 
-void Player::removeCard(Card c) {
+void Player::removeCard(shared_ptr<Card> c) {
     cout << "Removing card from player" << endl;
-    //TODO: remove card from player's hand here
+	Deck tempDeck;
+	playerHand->useCard(c, tempDeck);
 }
 
-void Player::addOrder(Order o) {
-    cout << "Adding order to player" << endl;
-    playerOrders->push_back(o);
-}
-
-void Player::removeOrder(Order o) {
-    cout << "Removing order from player" << endl;
-    playerOrders->remove(o);
-}
-
-Hand* Player::getHand() { 
+shared_ptr<Hand> Player::getHand() { 
     return playerHand; 
 }
 
-std::list<Map::Territory>* Player::getTerritories() { 
+std::list<shared_ptr<Map::Territory>> Player::getTerritories() { 
     return playerTerritories; 
 }
 
-std::list<Order>* Player::getOrders() { 
+std::list<shared_ptr<Order>> Player::getOrders() { 
     return playerOrders; 
 }
 
 Player& Player::operator=(const Player& other) {
+    
+	cout << "Player copy assignment operator called" << endl;
     if (this != &other) {
-        delete playerHand;
-        delete playerTerritories;
-        delete playerOrders;
-
-        playerHand = new Hand(*other.playerHand);
-        playerTerritories = new list<Map::Territory>(*other.playerTerritories);
-        playerOrders = new list<Order>(*other.playerOrders);
+        playerHand = other.playerHand;
+        playerTerritories = other.playerTerritories;
+        playerOrders = other.playerOrders;
     }
     return *this;
 }
 
 std::ostream& operator<<(std::ostream& os, const Player& p) {
-    os << "Player with " << p.playerTerritories->size() << " territories: ";
-    for(auto& territory : *p.playerTerritories) {
-        os << territory.getName() << " ";
+    os << "Player Territories:" << endl;
+    for (const auto& territory : p.playerTerritories) {
+        if (territory) {
+            os << "  " << territory << endl;
+        }
+        else {
+            os << "  None" << endl;
+        }
     }
-    os << p.playerOrders->size() << " orders, and "
-       << p.playerHand->size() << " cards in hand.";
+
+    os << "Player Orders:" << endl;
+    for (const auto& order : p.playerOrders) {
+        if (order) {
+            os << order << endl;
+        }
+        else {
+            os << " None" << endl;
+        }
+    }
+
+    os << "Player Hand:" << endl;
+    if (p.playerHand) {
+        auto handCards = p.playerHand->getHand();
+        for (const auto& card : handCards) {
+            if (card) {
+                os << card << endl;
+            }
+            else {
+                os << "  [null card]\n";
+            }
+        }
+    }
+    else {
+        os << "  No Cards" << endl;
+    }
+
     return os;
 }
--- player/playerDriver.cpp
+++ player/playerDriver.cpp
@@ -1,51 +1,76 @@
 #include "player.h"
 using namespace std;
 #include <iostream>
-
-void testPlayers();
-
-int main(){
-    cout << "Testing Player class" << endl;
-    testPlayers();
-};
+#include <memory>
 
 void testPlayers(){
-    
+
+	cout << "Testing Player class" << endl;
+
+  
     Player p1;
 
     // Add territories
-    Map::Territory t1("Territory1", "Continent1");
-    Map::Territory t2("Territory2", "Continent1");
+    std::shared_ptr<Map::Territory> t1 = std::make_shared<Map::Territory>("Territory1", "Continent1");
+    std::shared_ptr<Map::Territory> t2 = std::make_shared<Map::Territory>("Territory2", "Continent1");
     p1.addTerritory(t1);
     p1.addTerritory(t2);
 
     // Add cards
-    Card c1(CardType::BOMB);
-    Card c2(CardType::REINFORCEMENT);
+    std::shared_ptr<Card> c1 = std::make_shared<Card>(CardType::BOMB);
+    std::shared_ptr<Card> c2 = std::make_shared<Card>(CardType::REINFORCEMENT);
+
     p1.addCard(c1);
     p1.addCard(c2);
 
-    // Add orders
-    Order o1, o2;
-    p1.addOrder(o1);
-    p1.addOrder(o2);
+	shared_ptr<Order> o1 = make_shared<Order>(orders::orderType::BOMB);
+	shared_ptr<Order> o2 = make_shared<Order>(orders::orderType::AIRLIFT);
+	shared_ptr<Order> o3 = make_shared<Order>(orders::orderType::NEGOTIATE);
+
+    // Demonstrate issueOrder
+    cout << "Calling issueOrder:" << endl;
+    p1.issueOrder(o1);
+	p1.issueOrder(o2);
+	p1.issueOrder(o3);
 
     // Demonstrate toAttack and toDefend
     cout << "Player 1 toAttack and toDefend:" << endl;
     p1.toAttack();
     p1.toDefend();
 
-    // Demonstrate issueOrder
-    cout << "Calling issueOrder:" << endl;
-    p1.issueOrder();
-
     // Print number of orders
-    cout << "Player 1 has " << p1.getOrders()->size() << " orders" << endl;
+    cout << "Player 1 has " << p1.getOrders().size() << " orders" << endl;
 
     // Print cards in hand
     cout << "Player 1 has " << p1.getHand()->size() << " cards in hand" << endl;
 
     // Print territories
-    cout << "Player 1 has " << p1.getTerritories()->size() << " territories" << endl;
+    cout << "Player 1 has " << p1.getTerritories().size() << " territories" << endl;
+
+	Player p2; // Test copy assignment
+
+    p2 = p1;
+
+	Player p3(p1); // Test copy constructor
+
+	// Print number of orders
+    cout << "Player 2 has " << p2.getOrders().size() << " orders" << endl;
+
+    // Print cards in hand
+    cout << "Player 2 has " << p2.getHand()->size() << " cards in hand" << endl;
+
+    // Print territories
+    cout << "Player 2 has " << p2.getTerritories().size() << " territories" << endl;
+
+	// Print number of orders
+    cout << "Player 3 has " << p3.getOrders().size() << " orders" << endl;
+
+    // Print cards in hand
+    cout << "Player 3 has " << p3.getHand()->size() << " cards in hand" << endl;
+
+    // Print territories
+    cout << "Player 3 has " << p3.getTerritories().size() << " territories" << endl;
+
+	cout << p1 << endl;
 
 };
